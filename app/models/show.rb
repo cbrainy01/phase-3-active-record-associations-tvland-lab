9467cefec9e089c2411f7ed4ad9b59f63ab1d226
Carl <carlbrainegebe@gmail.com>
2021-08-01 16:38:33 -0500

    commit tree b54bac4ecd65a3a98d5f07ef916cf493b4e9217c

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors
+
+  def actors_list
+    # list_of_actor_names = []
+    self.characters.collect {|character| "#{character.actor.first_name} #{character.actor.last_name}"}
+    # list_of_actor_names
+  end
 end
